b72865d589d89f5c846650d5d5afe65b871e086a
Leonardoquinagliabatista <162454239+Leonardoquinagliabatista@users.noreply.github.com>
2025-08-29 11:56:16 -0300

    r

--- script.js
+++ script.js
@@ -6,54 +6,54 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você acha que o futuro será mais tecnológico ou mais sustentável??",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Tecnológico!",
+                afirmacao: "A tecnologia avançando em ritmo acelerado vai transformar completamente o modo como vivemos. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Sustentável!",
+                afirmacao: "O futuro precisa ser voltado para a preservação do meio ambiente, com soluções mais ecológicas e responsáveis."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Você acredita que o trabalho remoto será a norma no futuro??",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Sim.",
+                afirmacao: "A flexibilidade de trabalhar de qualquer lugar vai se tornar cada vez mais comum, permitindo mais equilíbrio entre vida pessoal e profissional."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Não.",
+                afirmacao: "A interação presencial e o ambiente de escritório ainda são importantes para a criatividade e colaboração em equipes."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Você preferiria viver em uma cidade futurista ou no campo?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Cidade futurista.",
+                afirmacao: "A conveniência e as tecnologias avançadas de uma cidade futurista são atraentes para quem gosta de inovação."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Campo",
+                afirmacao: "A tranquilidade e o contato com a natureza do campo podem ser ideais para quem busca um estilo de vida mais simples e pacífico."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você acha que a inteligência artificial terá mais impacto na educação ou na medicina?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Educação.",
+                afirmacao: "A IA pode personalizar o aprendizado e tornar a educação mais acessível e eficaz para todos."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Medicina",
+                afirmacao: "Com a IA, podemos ver avanços significativos em diagnósticos rápidos e tratamentos mais precisos"
             }
         ]
     },
